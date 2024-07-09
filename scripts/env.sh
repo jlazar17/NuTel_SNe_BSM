3f0cdf7d9add8745ce4478c75d42f49cc1d9f400
Jeffrey Lazar <jlazar@holy7c18212.rc.fas.harvard.edu>
2024-07-09 09:07:16 -0400

    export variables

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,8 @@
-SNE_BSM_PATH="/Users/jlazar/research/NuTel_SNe_BSM/"
-ASTERIA_PATH="/Users/jlazar/research/ASTERIA/"
+# Update this stuff
+export SNE_BSM_PATH="${HOME}/NuTel_SNe_BSM/"
+export ASTERIA_PATH="${HOME}/ASTERIA_git/"
+export DATADIR=/n/holylfs05/LABS/arguelles_delgado_lab/Everyone/jlazar/SNe_BSM/
 
+# Don't update this stuff
 export PYTHONPATH=$PYTHONPATH:$SNE_BSM_PATH:${ASTERIA_PATH}/python
 export ASTERIA=$ASTERIA_PATH
